7ac0b33a6d1fd7bc3fe89b409ea36c7137e50ce2
Matthew Morgan <mmorgan55@cnm.edu>
2018-10-17 09:01:28 -0600

    Fixed some bugs with displaying during fast run and began a new layout for the dice.

---
+++ app/src/main/java/edu/cnm/deepdive/crapssimulator/view/RollAdapter.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive.crapssimulator.view;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import edu.cnm.deepdive.crapssimulator.R;
+import edu.cnm.deepdive.crapssimulator.model.Game.Roll;
+
+public class RollAdapter extends ArrayAdapter<Roll> {
+
+  public RollAdapter(@NonNull Context context, int resource) {
+    super(context, resource);
+    //TODO Load cie images into a drawable array
+  }
+
+  @NonNull
+  @Override
+  public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    Roll roll = getItem(position);
+    View view = LayoutInflater.from(getContext()).inflate(R.layout.roll_item, parent, false);
+    //TODO Populate view objects
+    return  view;
+  }
+}
